454fdf42ce038d14a056a992c28127b0652e69f4
leequanno1 <binquan1234@gmail.com>
2024-12-14 21:42:04 +0700

    test done

--- book-service/src/app/routes/book-category.route.js
+++ book-service/src/app/routes/book-category.route.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const Controller = require("../controllers/book-category.controller")
+const Controller = require("../controllers/book-category.controller");
 
 router.get("/get-all", Controller.getAll);
 
-router.get("/get-total", Controller.getTottal);
+router.get("/get-total", Controller.getTotal);
 
 router.get("/get-count", Controller.getCount);
 
-router.get("/get-by-ids", Controller.getByIds);
+router.post("/post-get-by-ids", Controller.postGetByIds);
 
 router.get("/get-by-name", Controller.getByName);
 
